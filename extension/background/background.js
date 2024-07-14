3417681a00a98d5319352248d670428099568f90
Hiroya-W <hyuyukun@gmail.com>
2024-07-14 14:49:39 +0900

    :bug: sendResponseがundefinedを返すのでListenerではasync/awaitが使えない

--- extension/background/background.js
+++ extension/background/background.js
@@ -11,22 +11,28 @@ const defaultPreference = {
 
 const isEmptyObject = (obj) => Object.keys(obj).length === 0;
 
-chrome.runtime.onInstalled.addListener(async () => {
-  // initialize or migrate
-  const local = await chrome.storage.local.get(LOCAL_STORAGE_PREFERENCE_KEY);
+chrome.runtime.onInstalled.addListener(() => {
+  (async () => {
+    // initialize or migrate
+    const local = await chrome.storage.local.get(LOCAL_STORAGE_PREFERENCE_KEY);
 
-  const initialPreference = JSON.parse(
-    isEmptyObject(local)
-      ? '{"version": 0}'
-      : local[LOCAL_STORAGE_PREFERENCE_KEY]
-  );
+    const initialPreference = JSON.parse(
+      isEmptyObject(local)
+        ? '{"version": 0}'
+        : local[LOCAL_STORAGE_PREFERENCE_KEY]
+    );
 
-  if (initialPreference.version < 2) {
-    const migrated = { ...defaultPreference, ...initialPreference, version: 2 };
-    await chrome.storage.local.set({
-      [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.stringify(migrated),
-    });
-  }
+    if (initialPreference.version < 2) {
+      const migrated = {
+        ...defaultPreference,
+        ...initialPreference,
+        version: 2,
+      };
+      await chrome.storage.local.set({
+        [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.stringify(migrated),
+      });
+    }
+  })();
 });
 
 const tabIds = new Set();
@@ -36,41 +42,49 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   }
 });
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  console.log(request);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  (async () => {
+    console.log(request);
 
-  if ("hasTabId" in request) {
-    sendResponse({ result: tabIds.has(request.hasTabId) });
-    return;
-  }
-  if ("addTabId" in request) {
-    tabIds.add(request.addTabId);
-    return;
-  }
-  if ("getPreference" in request) {
-    const local = await chrome.storage.local.get(LOCAL_STORAGE_PREFERENCE_KEY);
-    const json = isEmptyObject(local)
-      ? "{}"
-      : local[LOCAL_STORAGE_PREFERENCE_KEY];
+    if ("hasTabId" in request) {
+      sendResponse({ result: tabIds.has(request.hasTabId) });
+      return;
+    }
+    if ("addTabId" in request) {
+      tabIds.add(request.addTabId);
+      return;
+    }
+    if ("getPreference" in request) {
+      const local = await chrome.storage.local.get(
+        LOCAL_STORAGE_PREFERENCE_KEY
+      );
+      const json = isEmptyObject(local)
+        ? "{}"
+        : local[LOCAL_STORAGE_PREFERENCE_KEY];
 
-    sendResponse({
-      [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.parse(json),
-    });
-    return;
-  }
-  if ("setPreference" in request) {
-    const local = await chrome.storage.local.get(LOCAL_STORAGE_PREFERENCE_KEY);
-    const json = isEmptyObject(local)
-      ? "{}"
-      : local[LOCAL_STORAGE_PREFERENCE_KEY];
-    const preference = { ...JSON.parse(json), ...request.setPreference };
-    await chrome.storage.local.set({
-      [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.stringify(preference),
-    });
-    sendMessage({ preference });
-    return;
-  }
-  sendMessage(request);
+      sendResponse({
+        [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.parse(json),
+      });
+      return;
+    }
+    if ("setPreference" in request) {
+      const local = await chrome.storage.local.get(
+        LOCAL_STORAGE_PREFERENCE_KEY
+      );
+      const json = isEmptyObject(local)
+        ? "{}"
+        : local[LOCAL_STORAGE_PREFERENCE_KEY];
+      const preference = { ...JSON.parse(json), ...request.setPreference };
+      await chrome.storage.local.set({
+        [LOCAL_STORAGE_PREFERENCE_KEY]: JSON.stringify(preference),
+      });
+      sendMessage({ preference });
+      return;
+    }
+    sendMessage(request);
+  })();
+
+  return true;
 });
 
 const sendMessage = (message) => {
